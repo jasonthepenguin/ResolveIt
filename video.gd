efbe10cb3aea25d4ac0d880b719bc0b90c318009
Jason Botterill <jasonbotterill@hotmail.com>
2024-09-16 20:20:56 +0800

    added intro video

---
+++ video.gd
@@ -0,0 +1,28 @@
+extends Control
+
+
+@export var play_intro = true
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	if play_intro == false:
+		
+		queue_free()
+		
+	
+	
+	pass # Replace with function body.
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	queue_free()
+	pass # Replace with function body.
