1f58bb600d2f854784d60cda2ab0d5518d7fce00
Jinxed Nation William Halling <32233703@student.murdoch.edu.au>
2024-10-24 22:41:14 +0800

    Emotion Emojis Added to AI
    Emotion Emojis Added to AI

--- AI_Movement.gd
+++ AI_Movement.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
 
 @onready var agent: NavigationAgent3D = $NavigationAgent3D
+
+	## Movement Variables
 var m_NPC_WalkSpeed = 1.5
 var m_Location = Vector3()
 
@@ -14,10 +16,25 @@ var back_away_direction = Vector3.ZERO
 var back_away_distance = 3.0
 var distance_backed_away = 0.0
 
+
+	## Provides reference to the Sprite3D for emojis
+@onready var emoji_sprite = $Sprite3D                      
+
+	## Emoji Textures
+var neutral_Emoji = load("res://Neutral.png")
+var happy_Emoji = load("res://Happy.png")
+var angry_Emoji = load("res://Angry.png")
+var sad_Emoji = load("res://Sad.png")
+
+var cycle_Emotion = 4.0	  ## Changes emotions 
+var emotionDuration = 0.0 ## Monitors amount of time passed since last change
+
 func _ready():
 	randomize()
 	move_to_random_location()
 
+	emoji_sprite.texture = neutral_Emoji
+	
 		## Debugging: Confirm that the NavigationAgent3D is active
 	if agent:
 		print("Navigation Agent Ready!")
@@ -25,7 +42,10 @@ func _ready():
 	else:
 		print("Navigation Agent Not Found!")
 
+
+
 func move_to_random_location():
+	
 	m_Location = getRandomLocation()
 	agent.set_target_position(m_Location)
 	
@@ -41,39 +61,45 @@ func getRandomLocation():
 	return Vector3(xLoc, 0, zLoc)
 
 
+func cycleEmotions():
+	if emoji_sprite.texture == neutral_Emoji:
+		emoji_sprite.texture = happy_Emoji
+	elif emoji_sprite.texture == happy_Emoji:
+		emoji_sprite.texture = angry_Emoji
+	elif emoji_sprite.texture == angry_Emoji:
+		emoji_sprite.texture = sad_Emoji
+	else:
+		emoji_sprite.texture = neutral_Emoji  # Cycle back to neutral
+		
+		
 func _process(delta):
 	
 	updateAI(delta)
-
+	emotionDuration += delta
+	if emotionDuration >= cycle_Emotion:
+		cycleEmotions()
+		emotionDuration = 0.0  # Reset the timer
+		
 
 func updateAI(delta):
 	
 	if collided:
-			## Decrease the timer after a collision
-		pause_timer -= delta
+		pause_timer -= delta												## Decrease the timer after a collision
 		
 		if pause_timer <= 0:
-				## Move away in the opposite direction after the pause
-			var move_vector = back_away_direction * m_NPC_WalkSpeed * delta
-			var collision = move_and_collide(move_vector)
+			var move_vector = back_away_direction * m_NPC_WalkSpeed * delta ## Move away in the opposite direction after the pause
+			var collision = move_and_collide(move_vector)	
+			distance_backed_away += move_vector.length()       				## Monitor how far we have moved away
 			
-				## Track how far we have moved away
-			distance_backed_away += move_vector.length()
-			
-				## Check if the agent has backed away far enough
-			if distance_backed_away >= back_away_distance or collision:
-				
+			if distance_backed_away >= back_away_distance or collision:		## Check if the agent has backed away far enough
 				collided = false
-				distance_backed_away = 0.0  # Reset the distance counter
-				
-				move_to_random_location()  # Move to a new random location after backing away
+				distance_backed_away = 0.0  								## Reset the distance counter
+				move_to_random_location()  									## Move to a new random location after backing away
 		return
 
 		## Check if navigation is finished and set new target location
 	if agent.is_navigation_finished():
-		
 		print("Navigation finished, moving to new location!")
-		
 		move_to_random_location()
 
 
@@ -93,7 +119,6 @@ func updateAI(delta):
 		print("Collision detected, pausing movement.")
 		collided = true
 		pause_timer = collision_pause_time
-
 			## Get the opposite direction based on the collision normal to back away
 		back_away_direction = -collision.get_normal().normalized()
 
