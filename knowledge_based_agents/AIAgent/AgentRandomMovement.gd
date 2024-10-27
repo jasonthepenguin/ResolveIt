9c2cad7cbef8281b1b71186059bd3c6fecd7351a
0V3RW4TC11 <102503090+0V3RW4TC11@users.noreply.github.com>
2024-10-27 21:28:15 +0800

    added scripts

---
+++ knowledge_based_agents/AIAgent/AgentRandomMovement.gd
@@ -0,0 +1,30 @@
+extends Node
+
+@export var max_stuck_time = 0.5
+@export var debug_info: bool = false
+@onready var character: CharacterBody3D = get_parent()
+@onready var nav_agent: NavigationAgent3D = character.get_node("NavigationAgent3D")
+@onready var actuator = character.get_node("NavActuator")
+var last_location: Vector3
+var stuck_time = 0.0
+
+func _ready():
+	randomize()
+
+func _physics_process(delta):
+	if (nav_agent.is_navigation_finished()):
+		actuator.move_to(get_random_position())
+	
+	var current_location = character.global_transform.origin
+	
+	if (last_location == current_location):
+		stuck_time += delta
+		if (stuck_time > max_stuck_time):
+			if debug_info: print("Got stuck")
+			actuator.move_to(get_random_position())
+			stuck_time = 0
+			
+	last_location = current_location
+
+func get_random_position() -> Vector2:
+	return Vector2(randf_range(-10, 10), randf_range(-10, 10))
---
+++ knowledge_based_agents/AIAgent/CharacterBodyImpulse.gd
@@ -0,0 +1,21 @@
+extends Node
+
+@export var push_force = 2.0
+@onready var character = get_parent()
+
+func _physics_process(_delta):
+	if not character.is_on_floor_only():
+		apply_impulse()
+
+func apply_impulse():
+	for i in character.get_slide_collision_count():
+		var collision = character.get_slide_collision(i)
+		if collision.get_collider() is RigidBody3D:
+			var rb = collision.get_collider() as RigidBody3D
+			var collision_point = collision.get_position()
+			var impulse = -collision.get_normal() * push_force
+
+			# Convert world collision point to position relative to RigidBody's center
+			var point_relative = collision_point - rb.global_position
+
+			rb.apply_impulse(impulse, point_relative)
