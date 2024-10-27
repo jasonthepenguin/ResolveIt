cb304d17ee7f0f077cbbe9c563ca539d2b548491
Jinxed Nation William Halling <32233703@student.murdoch.edu.au>
2024-10-27 08:04:33 +0800

    AI_Movement with model

--- AI_Movement.gd
+++ AI_Movement.gd
@@ -2,20 +2,21 @@ extends CharacterBody3D
 
 @onready var agent: NavigationAgent3D = $NavigationAgent3D
 @onready var emoji_controller: Sprite3D = $Sprite3D
-@onready var animation_player: AnimationPlayer = $Rogue_Hooded/AnimationPlayer
-@onready var character_model: Node3D = $Rogue_Hooded
+@onready var animation_player: AnimationPlayer = $Rogue_Hooded2/AnimationPlayer
+@onready var character_model: Node3D = $Rogue_Hooded2
 
 	## Movement Variables
 var m_NPC_WalkSpeed = 1.5
 var m_Location = Vector3()
 
+
 	## Time to pause after collision (in seconds)
-var collision_pause_time = 1.0
-var pause_timer = 0.0
 var collided = false
-var back_away_direction = Vector3.ZERO
+var pause_timer = 0.0
 var back_away_distance = 3.0
+var collision_pause_time = 1.0
 var distance_backed_away = 0.0
+var back_away_direction = Vector3.ZERO
 
 
 	## Detection Variables
@@ -23,7 +24,8 @@ var distance_backed_away = 0.0
 var current_target: Node3D = null
 var is_interacting: bool = false
 
-# Add animation states
+
+	## Add animation states
 enum AnimationState {
 	IDLE,
 	WALK,
@@ -48,7 +50,6 @@ func _ready():
 	
 
 func play_animation(anim_state: AnimationState):
-	
 	if current_animation == anim_state:
 		return
 		
@@ -59,12 +60,16 @@ func play_animation(anim_state: AnimationState):
 		AnimationState.WALK:
 			animation_player.play("Walking_A")
 		AnimationState.INTERACT:
-			animation_player.play("Use_Item")
-			
+			if emoji_controller.texture == emoji_controller.angry_Emoji:
+				animation_player.play("PickUp")
+			else:
+				animation_player.play("Use_Item")
+
 
 func update_model_rotation(direction: Vector3):
 	if direction != Vector3.ZERO:
-		character_model.look_at(global_position + direction)
+		var angle = atan2(direction.x, direction.z)
+		character_model.rotation.y = angle
 
 
 
@@ -85,7 +90,6 @@ func getRandomLocation():
 func find_nearest_interactable():
 	var nearest = INF
 	var nearestObj = null
-	
 	var interactables = get_tree().get_nodes_in_group("interactable")
 
 	for obj in interactables:
@@ -101,76 +105,171 @@ func find_nearest_interactable():
 
 func handle_interaction():
 	if current_target and current_target.has_node("Affordance"):
-		
 		var affordance = current_target.get_node("Affordance")
-		
 		print("Found cube with affordance: ", current_target.name)  # Debug print
 		
 
 		if affordance.has_affordance("activate"):
-		
-			print("Triggering activate affordance")
+			print("AI Emotion: ", "Angry" if emoji_controller.texture == emoji_controller.angry_Emoji else "Happy/Neutral")
+			play_animation(AnimationState.INTERACT)
 			affordance.trigger_affordance("activate")
 		
 		is_interacting = true
 		await get_tree().create_timer(2.0).timeout
 		is_interacting = false
 		current_target = null
-		
-			
-func updateAI(delta):
 	
-	if not is_interacting:
 		
-			## Look for interactable objects first when angry
+func handle_movement(delta):
+	var nextLocation = agent.get_next_path_position()
+	var travelDirection = (nextLocation - global_transform.origin).normalized()
+	
+	update_model_rotation(travelDirection)
+	play_animation(AnimationState.WALK)
+	
+	var collision = move_and_collide(travelDirection * m_NPC_WalkSpeed * delta)
+	return collision
+
+
+func handle_collision(collision):
+	print("Collision detected, pausing")
+	collided = true
+	pause_timer = collision_pause_time
+	back_away_direction = -collision.get_normal().normalized()
+	play_animation(AnimationState.IDLE)
+
+
+func handle_collision_state(delta):
+	pause_timer -= delta
+	if pause_timer <= 0:
+		var move_vector = back_away_direction * m_NPC_WalkSpeed * delta
+		var collision = move_and_collide(move_vector)
+		distance_backed_away += move_vector.length()
+		
+		if distance_backed_away >= back_away_distance or collision:
+			collided = false
+			distance_backed_away = 0.0
+			move_to_random_location()
+
+
+func updateAI(delta):
+	if not is_interacting:
 		if emoji_controller.texture == emoji_controller.angry_Emoji:
-			
+			print("AI is Angry - Looking for objects to interact with")
+			# When angry, look for objects to interact with
 			var nearest_interactable = find_nearest_interactable()
-			
 			if nearest_interactable:
 				var distance = global_position.distance_to(nearest_interactable.global_position)
-			
-				if distance <= 1.5:  ## Close enough to interact
+				if distance <= 1.5:
 					current_target = nearest_interactable
-					handle_interaction()
-					
-					return
+					handle_angry_interaction()
 				else:
-						## Move towards the interactable
 					agent.set_target_position(nearest_interactable.global_position)
-					
-	if collided:
-		pause_timer -= delta
-		
-		if pause_timer <= 0:
+			else:
+				if agent.is_navigation_finished():
+					print("Angry AI - No interactable nearby, moving to new location!")
+					move_to_random_location()
+			var collision = handle_movement(delta)
+			if collision:
+				handle_collision(collision)
+
+		elif emoji_controller.texture == emoji_controller.happy_Emoji:
+			print("AI is Happy - Looking to make things green!")
+			# When happy, look for objects to turn green
+			var nearest_interactable = find_nearest_interactable()
+			if nearest_interactable:
+				var distance = global_position.distance_to(nearest_interactable.global_position)
+				if distance <= 1.5:
+					current_target = nearest_interactable
+					handle_happy_interaction()  # Turn object green
 			
-			var move_vector = back_away_direction * m_NPC_WalkSpeed * delta
-			var collision = move_and_collide(move_vector)
-			distance_backed_away += move_vector.length()
-		
-			if distance_backed_away >= back_away_distance or collision:
-				collided = false
-				distance_backed_away = 0.0
+			# Casual roaming
+			if agent.is_navigation_finished():
+				print("Happy AI - Casually moving to new location")
 				move_to_random_location()
-		return
-	
-		## Normal movement when not angry or no interactables nearby
-	if agent.is_navigation_finished():
-		print("Navigation finished, moving to new location!")
-		move_to_random_location()
-		
-	else:
-		var nextLocation = agent.get_next_path_position()
-		var travelDirection = (nextLocation - global_transform.origin).normalized()
-		var collision = move_and_collide(travelDirection * m_NPC_WalkSpeed * delta)
-		
-		
-		if collision:
-			print("Collision detected, pausing movement.")
-			collided = true
-			pause_timer = collision_pause_time
-			back_away_direction = -collision.get_normal().normalized()
+			var collision = handle_movement(delta)
+			if collision:
+				handle_collision(collision)
+
+		elif emoji_controller.texture == emoji_controller.sad_Emoji:
+			print("AI is Sad - Running to corner")
+			# When sad, run to nearest corner and look around
+			move_to_corner()
+			rotate_sad()
 			
+		elif emoji_controller.texture == emoji_controller.neutral_Emoji:
+			print("AI is Neutral - Walking around")
+			# When neutral, just walk around
+			if agent.is_navigation_finished():
+				print("Navigation finished, moving to new location!")
+				move_to_random_location()
+			var collision = handle_movement(delta)
+			if collision:
+				handle_collision(collision)
+	
+	if collided:
+		handle_collision_state(delta)
+
+# Add these new functions for handling different emotional interactions
+func handle_angry_interaction():
+	if current_target and current_target.has_node("Affordance"):
+		var affordance = current_target.get_node("Affordance")
+		if affordance.has_affordance("activate"):
+			print("Angry - turning object red")
+			play_animation(AnimationState.INTERACT)
+			affordance.trigger_affordance("activate")  # Default red color
+		
+		is_interacting = true
+		await get_tree().create_timer(2.0).timeout
+		is_interacting = false
+		current_target = null
+
+
+func handle_happy_interaction():
+	if current_target and current_target.has_node("Affordance"):
+		var affordance = current_target.get_node("Affordance")
+		if affordance.has_affordance("activate"):
+			print("Happy - turning object green")
+			play_animation(AnimationState.INTERACT)
+		is_interacting = true
+		await get_tree().create_timer(2.0).timeout
+		is_interacting = false
+		current_target = null
+		
+
+func move_to_corner():
+	# Find nearest corner based on your room dimensions
+	var corners = [
+		Vector3(-8.7, 0, 7),    # Back left
+		Vector3(2.7, 0, 7),     # Back right
+		Vector3(-8.7, 0, 22.5), # Front left
+		Vector3(2.7, 0, 22.5)   # Front right
+	]
+	
+	var nearest_corner = corners[0]
+	var nearest_dist = global_position.distance_to(corners[0])
+	
+	for corner in corners:
+		var dist = global_position.distance_to(corner)
+		if dist < nearest_dist:
+			nearest_dist = dist
+			nearest_corner = corner
+	
+	agent.set_target_position(nearest_corner)
+
+func rotate_sad():
+	if not is_interacting:
+		# Create a tween for smooth rotation
+		var tween = create_tween()
+		tween.tween_property(character_model, "rotation:y", PI/4, 1.0)  # Look right
+		await tween.finished
+		tween = create_tween()
+		tween.tween_property(character_model, "rotation:y", -PI/4, 1.0)  # Look left
+		await tween.finished
+		
+		# Repeat the rotation if still sad
+		if emoji_controller.texture == emoji_controller.sad_Emoji:
+			rotate_sad()
 			
 func _physics_process(delta):
 	updateAI(delta)
--- EmojiManager.gd
+++ EmojiManager.gd
@@ -1,5 +1,12 @@
-# EmojiController.gd
 extends Sprite3D
+
+## Enum for Emotions
+enum Emotions {
+	NEUTRAL,
+	HAPPY,
+	ANGRY,
+	SAD
+}
 
 ## Emoji Textures
 var neutral_Emoji = load("res://Neutral.png")
@@ -7,50 +14,69 @@ var happy_Emoji = load("res://Happy.png")
 var angry_Emoji = load("res://Angry.png")
 var sad_Emoji = load("res://Sad.png")
 
+## Mapping Emotions to Textures
+var emotion_textures = {
+	Emotions.NEUTRAL: neutral_Emoji,
+	Emotions.HAPPY: happy_Emoji,
+	Emotions.ANGRY: angry_Emoji,
+	Emotions.SAD: sad_Emoji
+}
 
-var emotionDuration = 0.0  		 ## Monitors time passed since last change
-var force_emotion: bool = false  ## Prevents cycling of emotions
+var current_emotion = Emotions.NEUTRAL
 
-@export var cycle_Emotion = 4.0  ## Changes emotions every 4 seconds
+var emotionDuration = 0.0         ## Monitors time passed since last change
+var force_emotion: bool = false   ## Prevents cycling of emotions
+
+# Custom durations for each emotion (in seconds)
+var emotion_durations = {
+	Emotions.NEUTRAL: 6.0,
+	Emotions.HAPPY: 10.0,
+	Emotions.ANGRY: 7.0,
+	Emotions.SAD: 10.0
+}
 
 func _ready():
 	randomize()
-	var emotions = [neutral_Emoji, happy_Emoji, angry_Emoji, sad_Emoji]
-	self.texture = emotions[randi() % emotions.size()]
+	var emotions = [Emotions.NEUTRAL, Emotions.HAPPY, Emotions.ANGRY, Emotions.SAD]
+	current_emotion = emotions[randi() % emotions.size()]
+	update_texture()
+	emotionDuration = 0.0
 
+func update_texture():
+	self.texture = emotion_textures[current_emotion]
 
 func cycleEmotions():
 	if force_emotion:
 		return
 
-	if self.texture == neutral_Emoji:
-		self.texture = happy_Emoji
-		
-	elif self.texture == happy_Emoji:
-		self.texture = angry_Emoji
-	
-	elif self.texture == angry_Emoji:
-		self.texture = sad_Emoji
-	
-	else:
-		self.texture = neutral_Emoji  ## Cycle back to neutral
+	match current_emotion:
+		Emotions.NEUTRAL:
+			current_emotion = Emotions.HAPPY
+		Emotions.HAPPY:
+			current_emotion = Emotions.ANGRY
+		Emotions.ANGRY:
+			current_emotion = Emotions.SAD
+		Emotions.SAD:
+			current_emotion = Emotions.NEUTRAL
+		_:
+			current_emotion = Emotions.NEUTRAL  # Default to neutral
 
+	update_texture()
+	emotionDuration = 0.0  ## Reset the timer
 
 func setEmotion(emotion):
-	self.texture = emotion
-	
+	current_emotion = emotion
+	update_texture()
 	force_emotion = true
-	
+	emotionDuration = 0.0  ## Reset the timer
+
 	await get_tree().create_timer(4.0).timeout
 	force_emotion = false
-
 
 func _process(delta):
 	if not force_emotion:
 		emotionDuration += delta
-		
-		
-	if emotionDuration >= cycle_Emotion:
 
+	var current_duration = emotion_durations.get(current_emotion, 4.0)
+	if emotionDuration >= current_duration:
 		cycleEmotions()
-		emotionDuration = 0.0  ## Reset the timer
