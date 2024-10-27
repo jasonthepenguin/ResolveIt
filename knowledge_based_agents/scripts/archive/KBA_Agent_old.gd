cb7b80b1fd3b088d29804ccbe31f81e0ee95aacc
0V3RW4TC11 <102503090+0V3RW4TC11@users.noreply.github.com>
2024-10-27 21:25:00 +0800

    Merge add-kba-nodes into FEATURE-AI-Agent (#15)
    * added AgentKnowledgeBase node
    
    * fixes to AgentKnowledgeBase.gd
    
    * renamed knowledge_base.gd to AgentKnowledgeBase.gd
    
    * revision of KBA KnowledgeBase
    
    * removed old kb script
    
    * added actions
    
    * knowledge base revision
    
    * added StudentAgent
    
    * updated agent nodes
    
    * added BaseAgent
    
    * added inspector options for agent tests

---
+++ knowledge_based_agents/scripts/archive/KBA_Agent_old.gd
@@ -0,0 +1,21 @@
+class_name KBA_Agent extends Node
+
+@export var action_list : Array[KBA_ActionNode]
+
+var perceptions = {}
+var actions = {}
+
+func _init():
+	pass
+
+func act(perception: Dictionary):
+	pass
+	
+func process_decisions():
+	pass
+	
+func _update_KB():
+	pass
+	
+func _decide_act():
+	pass
