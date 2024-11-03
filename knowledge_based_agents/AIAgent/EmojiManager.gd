30412988465e05bdbbdb08cd5315e8c4bfea1270
0V3RW4TC11 <102503090+0V3RW4TC11@users.noreply.github.com>
2024-11-03 12:24:34 +0800

    got teacher teaching-wander cycle working

--- knowledge_based_agents/AIAgent/EmojiManager.gd
+++ knowledge_based_agents/AIAgent/EmojiManager.gd
@@ -1,6 +1,6 @@
 # re-work of Will Hallings EmojiManager.gd
 
-extends Sprite3D
+class_name EmojiManager extends Sprite3D
 
 ## Emoji Textures
 var neutral_Emoji = load("res://Neutral.png")
@@ -8,19 +8,21 @@ var happy_Emoji = load("res://Happy.png")
 var angry_Emoji = load("res://Angry.png")
 var sad_Emoji = load("res://Sad.png")
 
+## Emotion states
+enum State {
+	NEUTRAL,
+	HAPPY,
+	ANGRY,
+	SAD
+}
+
 ## Mapping Emotions to Textures
 var emotion_textures = {
-	FuzzyEmotionTriangle.Emotion.NEUTRAL: neutral_Emoji,
-	FuzzyEmotionTriangle.Emotion.HAPPY: happy_Emoji,
-	FuzzyEmotionTriangle.Emotion.ANGRY: angry_Emoji,
-	FuzzyEmotionTriangle.Emotion.SAD: sad_Emoji
+	State.NEUTRAL: neutral_Emoji,
+	State.HAPPY: happy_Emoji,
+	State.ANGRY: angry_Emoji,
+	State.SAD: sad_Emoji
 }
 	
-func display_emotion(emotion: FuzzyEmotionTriangle.Emotion):
-	var selected
-	match emotion:
-		FuzzyEmotionTriangle.Emotion.NEUTRAL: selected = neutral_Emoji
-		FuzzyEmotionTriangle.Emotion.HAPPY: selected = happy_Emoji
-		FuzzyEmotionTriangle.Emotion.ANGRY: selected = angry_Emoji
-		FuzzyEmotionTriangle.Emotion.SAD: selected = sad_Emoji
-	self.texture = selected
+func display_emotion(state: State):
+	self.texture = emotion_textures[state]
